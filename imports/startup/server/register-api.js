4bf7928e8098504ee02c7ca9c120ef4046751a48
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-09-09 17:24:10 +0300

    adasd

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -15,8 +15,11 @@ import ResolutionResolvers from '../../api/resolutions/resolvers'
 import GoalSchema from '../../api/goals/Goal.graphql'
 import GoalResolvers from '../../api/goals/resolvers'
 
-// Reresh
+var events = require('events'),
+a = new events.EventEmitter();
+a.setMaxListeners(0);
 
+// Rere
 const typeDefs = [
     UserSchema,
     SubscribersSchema,
